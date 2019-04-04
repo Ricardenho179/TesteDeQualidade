2fd2094318ba42ace7c04cc037f62c25ad9d2163
Tecnico - TI <tecnico@epfserver.com.br>
2019-04-03 21:41:40 -0300

    primeiro commit

---
+++ test/FormularioTest.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.chrome.ChromeDriver;
+// não sei oque se poem acima.
+
+public class FormularioTest {
+	//esse cara não tem instância aceitável nem criada e não sei como se cria.
+	private WebDriver driver = null;
+	
+	@Before
+	public void setUp() throws Exception {
+	
+	
+	
+	}
+	
+	
+	@Test
+	public void deveCadastrarUmUsuarioComSucesso() {
+		
+	}
+	
+	
+	@After
+	public void posCondicao() throws Exception {
+		driver.quit();
+	}
+}
